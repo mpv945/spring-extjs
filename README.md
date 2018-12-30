117dfb8b11cdf5bbb23456648464337aa9675d35
zhenxindefeng@126.com <Xiehaijun945>
2018-12-31 02:30:35 +0800

    初始化项目结构

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # spring-extjs
 我的extjs项目
+
+## 官网教程(https://docs.sencha.com/extjs/6.6.0/guides/getting_started/getting_started.html)
+## cmd 使用教程（https://docs.sencha.com/cmd/6.6.0/）
+
+## 构建web项目 https://blog.csdn.net/p312011150/article/details/72723257
---
+++ src/main/java/com/haijun/study/springextjs/SpringExtjsApplication.java
@@ -0,0 +1,14 @@
+package com.haijun.study.springextjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExtjsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExtjsApplication.class, args);
+    }
+
+}
+
